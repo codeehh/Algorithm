bbe38b90124f00bcaa593c0cfd0dfa522146faf2
codehhhh <87011085+codehhhh@users.noreply.github.com>
2021-07-25 16:13:46 +0900

    Add files via upload

---
+++ DFS.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int N = 5;
+vector<int> adj[N];
+bool visited[N] = {false};
+void dfs(int node)
+{
+    if (visited[node])
+        return;
+    visited[node] = true;
+    cout << node << ' ';
+    for (auto x : adj[node])
+    {
+        dfs(x);
+    }
+    return;
+}
+
+int main(void)
+{
+    adj[0].push_back(1);
+    adj[0].push_back(3);
+    adj[1].push_back(0);
+    adj[1].push_back(2);
+    adj[1].push_back(4);
+    adj[2].push_back(1);
+    adj[2].push_back(4);
+    adj[3].push_back(0);
+    adj[4].push_back(1);
+    adj[4].push_back(2);
+    cout << "DFS" << endl;
+    dfs(0);
+    return 0;
+}
